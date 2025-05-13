e6c72b586059fd0023b77402bef9bb1d6f273003
Amit Pandey <amitpande184@gmail.com>
2025-05-13 11:10:22 +0530

    update env  Frontend  URl

--- Backend/app.js
+++ Backend/app.js
@@ -3,8 +3,10 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const userroutes = require('./routes/userroutes')
 const adminroutes = require('./routes/adminroutes')
+const path = require("path");
 
 const app = express();
+app.use(express.static(path.join(__dirname, "dist")));
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     credentials: true
