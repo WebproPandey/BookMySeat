09118ac9a11caac0ff5f027511524a0aca766e55
Amit Pandey <amitpande184@gmail.com>
2025-05-13 11:28:46 +0530

    Update backend env deploy url and app.js

--- Backend/app.js
+++ Backend/app.js
@@ -8,7 +8,7 @@ const path = require("path");
 const app = express();
 app.use(express.static(path.join(__dirname, "dist")));
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin:[process.env.FRONTEND_URL, "http://localhost:5173"],
     credentials: true
   }));
 
